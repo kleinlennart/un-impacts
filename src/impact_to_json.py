14e7f6f1ee1180a6f2c667d63be34302f6f18e78
kleinlennart <31488915+kleinlennart@users.noreply.github.com>
2025-11-07 11:45:03 -0500

    simplify

--- src/impact_to_json.py
+++ src/impact_to_json.py
@@ -1,4 +1,3 @@
-import json
 from pathlib import Path
 
 import pandas as pd
@@ -9,10 +8,6 @@ data_folder = Path("data")
 csv_path = data_folder / "input" / "impacts.csv"
 df = pd.read_csv(csv_path)
 
-# convert to JSON
-json_data = df.to_dict("records")
-
 # export JSON
 output_path = Path("public") / "data" / "impacts.json"
-with open(output_path, "w", encoding="utf-8") as f:
-    json.dump(json_data, f, indent=2, ensure_ascii=False)
+df.to_json(output_path, orient="records", indent=2)

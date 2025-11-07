7757ec0049ffa993d8a686519ed22fa6aacf615d
kleinlennart <31488915+kleinlennart@users.noreply.github.com>
2025-11-07 14:32:28 -0500

    update font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,13 @@
 import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
 import "./globals.css";
+
+// https://fonts.google.com/specimen/Roboto
+// 100 (Thin), 300 (Light), 400 (Regular), 500 (Medium), 700 (Bold), 800 (ExtraBold), 900 (Black)
+const roboto = Roboto({
+    weight: ['100', '300', '400', '500', '700', '800', '900'],
+    subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
     title: "UN Impacts",
@@ -12,7 +20,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
+        <html lang="en" className={`${roboto.className} antialiased`}>
             <body>
                 {children}
             </body>
